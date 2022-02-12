2243bd83aa4b1d4d2a1e194149e0b75c2a65f7f1
Reichemann <excellenceissoon@gmail.com>
2022-02-12 12:47:46 +0200

    Added problem solution

--- src/com/flameshine/nebula/NewYearChaos.java
+++ src/com/flameshine/nebula/NewYearChaos.java
@@ -1,7 +1,8 @@
-package app;
+package com.flameshine.nebula;
 
 /**
  * Problem statement:
+ *
  * It's New Year's Day and everyone's in line for the Wonderland rollercoaster ride!
  * There are a number of people queued up, and each person wears a sticker indicating their initial position in the queue.
  * Initial positions increment by 1 from 1 at the front of the line to at the back.
@@ -12,20 +13,22 @@ package app;
  * Fascinated by this chaotic queue, you decide you must know the minimum number of bribes that took place to get the queue into its current state!
  *
  * Function description:
+ *
  * Complete the function minimumBribes in the editor below.
- * It must print an integer representing the minimum number of bribes necessary, or "too chaotic" if the line configuration is not possible.
+ * It must print an integer representing the minimum number of bribes necessary, or "Too chaotic" if the line configuration is not possible.
  * The function has the following parameter(s): numbers - an array of integers.
  *
  * Output format:
+ *
  * Print an integer denoting the minimum number of bribes needed to get the queue into its final state.
- * Print "too chaotic" if the state is invalid, i.e. it requires a person to have bribed more than 2 people.
+ * Print "Too chaotic" if the state is invalid, i.e. it requires a person to have bribed more than 2 people.
  */
 
-public class NewYearChaos {
+public final class NewYearChaos {
 
     public static void main(String... args) {
 
-        var numbers = new int[] { 1, 2, 3, 5, 4, 6, 7, 8 };
+        var numbers = new int[] { 2, 1, 3, 5, 4, 8, 6, 7 };
 
         System.out.println(
             minimumBribes(numbers)
@@ -33,6 +36,24 @@ public class NewYearChaos {
     }
 
     private static int minimumBribes(int[] numbers) {
-        return 0;
+
+        var result = 0;
+
+        for (var i = 0; i < numbers.length; ++i) {
+
+            var distanceToInitialPosition = numbers[i] - (i + 1);
+
+            if (distanceToInitialPosition > 2) {
+                throw new IllegalStateException("Too chaotic");
+            }
+
+            for (var j = Math.max(0, numbers[i] - 2); j < i; ++j) {
+                if (numbers[j] > numbers[i]) {
+                    ++result;
+                }
+            }
+        }
+
+        return result;
     }
 }
