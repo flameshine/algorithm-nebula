1e9b96727e815c13d25ae8dc8fab77138baa270a
Reichemann <excellenceissoon@gmail.com>
2022-04-10 16:05:22 +0300

    Added a resolution of the one more problem

---
+++ src/com/flameshine/nebula/CountingWordsWithAGivenPrefix.kt
@@ -0,0 +1,32 @@
+package com.flameshine.nebula
+
+/**
+ * Problem statement:
+ *
+ * You're given an array of strings words and a string pref.
+ * Return the number of strings in words that contain pref as a prefix.
+ * A prefix of a string s is any leading contiguous substring of s.
+ */
+
+fun main() {
+
+    val numbers = arrayOf("pay", "attention", "practice", "attend")
+
+    println(
+        prefixCount(numbers, "at")
+    )
+}
+
+private fun prefixCount(words: Array<String>, pref: String): Int {
+
+    var counter = 0
+
+    for (word in words) {
+
+        if (word.startsWith(pref)) {
+            ++counter
+        }
+    }
+
+    return counter
+}
