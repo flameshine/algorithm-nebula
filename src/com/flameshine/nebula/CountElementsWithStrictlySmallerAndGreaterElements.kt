602175f58a4bd953f16a69ad4fdbdb9085ff980f
Reichemann <excellenceissoon@gmail.com>
2022-04-16 17:05:57 +0300

    Added a resolution of the one more problem

---
+++ src/com/flameshine/nebula/CountElementsWithStrictlySmallerAndGreaterElements.kt
@@ -0,0 +1,21 @@
+package com.flameshine.nebula
+
+/**
+ * Given an integer array nums, return the number of elements that have both a strictly smaller and a strictly greater element appear in nums.
+ */
+
+fun main() {
+
+    val nums = intArrayOf(-65, -65, 50, -65, 50, -55, -65, -65)
+
+    println(
+        countElements(nums)
+    )
+}
+
+private fun countElements(nums: IntArray): Int {
+
+    nums.sort()
+
+    return nums.count { it != nums[0] && it != nums[nums.size - 1]}
+}
