187ed92c7ea30d7047932e66dcf1696dd6749e76
Reichemann <excellenceissoon@gmail.com>
2022-04-24 15:11:51 +0300

    Added a resolution of another batch of problems

---
+++ src/com/flameshine/nebula/DefangingIpAddress.kt
@@ -0,0 +1,24 @@
+package com.flameshine.nebula
+
+/**
+ * Given a valid (IPv4) IP address, return a defanged version of that IP address.
+ * A defanged IP address replaces every period "." with "[.]".
+ */
+
+fun main() {
+
+    println(
+        defangIpAddress("1.1.1.1")
+    )
+}
+
+private fun defangIpAddress(address: String): String {
+
+    val result = StringBuilder()
+
+    for (i in address.indices) {
+        if (address[i] == '.') result.append("[.]") else result.append(address[i])
+    }
+
+    return result.toString()
+}
---
+++ src/com/flameshine/nebula/HappyNumber.kt
@@ -0,0 +1,37 @@
+package com.flameshine.nebula
+
+/**
+ * Write an algorithm to determine if a number n is happy.
+ * A happy number is a number defined by the following process:
+ *  - Starting with any positive integer, replace the number by the sum of the squares of its digits
+ *  - Repeat the process until the number equals 1 (where it will say), or it loops endlessly in a cycle which does not include 1
+ *  - Those numbers for which this process ends in 1 are happy
+ *  Return true if n is a happy number and false if not.
+ */
+
+fun main() {
+
+    println(
+        isHappy(19)
+    )
+}
+
+private fun isHappy(n: Int): Boolean = isHappyWithMemory(n, HashSet())
+
+private fun isHappyWithMemory(n: Int, memory: HashSet<Int>): Boolean {
+
+    if (memory.contains(n)) return false
+
+    memory.add(n)
+
+    val stringified = n.toString()
+
+    return when (val sumOfDigits = List(stringified.length) { powInt(stringified[it].digitToInt()) }.sum()) {
+        1 -> true
+        else -> {
+            isHappyWithMemory(sumOfDigits, memory)
+        }
+    }
+}
+
+private fun powInt(n: Int): Int = n * n
