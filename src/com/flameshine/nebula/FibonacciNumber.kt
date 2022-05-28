4c890ba5b9617d64cfd092e8062600f4d7c8f7ad
Reichemann <excellenceissoon@gmail.com>
2022-05-28 15:27:05 +0300

    Added a resolution of another batch of problems

---
+++ src/com/flameshine/nebula/FibonacciNumber.kt
@@ -0,0 +1,19 @@
+package com.flameshine.nebula
+
+/**
+ * Problem statement:
+ *
+ * The Fibonacci numbers, commonly denoted F(n) from a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1.
+ * Given n, calculate F(n).
+ */
+
+fun main() {
+
+    println(
+        fibonacci(4)
+    )
+}
+
+fun fibonacci(n: Int): Int {
+    return if (n <= 1) n else fibonacci(n - 1) + fibonacci(n - 2)
+}
