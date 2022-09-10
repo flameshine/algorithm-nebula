e704f4bbf6f0593fd0c8f61e4cb19e3ee111fb41
Reichemann <excellenceissoon@gmail.com>
2022-09-10 13:33:15 +0300

    One more problem has been resolved

---
+++ src/com/flameshine/nebula/ReverseWordsInAString.kt
@@ -0,0 +1,48 @@
+package com.flameshine.nebula
+
+/**
+ * Problem statement:
+ *
+ * Given a string 's', reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+ */
+
+fun main() {
+
+    val s = "Let's take LeetCode contest"
+
+    println(
+        reverseWords(s)
+    )
+}
+
+private fun reverseWords(s: String): String {
+
+    val words = arrayListOf<String>()
+
+    for (word in s.split(" ")) {
+
+        words.add(
+            reverseString(word)
+        )
+    }
+
+    return words.joinToString(" ")
+}
+
+private fun reverseString(s: String): String {
+
+    val characters = s.toCharArray()
+
+    var start = 0
+    var end = s.length - 1
+
+    while (end > start) {
+        val temporary = characters[start]
+        characters[start] = characters[end]
+        characters[end] = temporary
+        ++start
+        --end
+    }
+
+    return String(characters)
+}
