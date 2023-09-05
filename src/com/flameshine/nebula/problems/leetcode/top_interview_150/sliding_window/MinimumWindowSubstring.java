805347be03c79ac8237447bd93456e203009f25a
Reichemann <excellenceissoon@gmail.com>
2023-09-06 02:05:38 +0300

    Solved a bunch of problems + minor adjustments

---
+++ src/com/flameshine/nebula/problems/leetcode/top_interview_150/sliding_window/MinimumWindowSubstring.java
@@ -0,0 +1,23 @@
+package com.flameshine.nebula.problems.leetcode.top_interview_150.sliding_window;
+
+/**
+ * №76 Minimum Window Substring
+ *
+ * Given two strings s and t of lengths m and n respectively, return the minimum window substring of s such that every character in t (including duplicates) is included in the window.
+ * If there is no such substring, return the empty string "".
+ * The testcases will be generated such that the answer is unique.
+ */
+public class MinimumWindowSubstring {
+
+    public static void main(String... args) {
+        System.out.println(
+            minWindow("ADOBECODEBANC", "ABC")
+        );
+    }
+
+    // TODO: complete
+
+    private static String minWindow(String s, String t) {
+        throw new UnsupportedOperationException();
+    }
+}
