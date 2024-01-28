4aed9253f89f282a700ce7be9adecaed516aef34
Reichemann <excellenceissoon@gmail.com>
2024-01-28 19:28:26 +0100

    Another pack of solved problems

---
+++ src/com/flameshine/nebula/problems/leetcode/learn/bst/ValidateBinarySearchTree.java
@@ -0,0 +1,69 @@
+package com.flameshine.nebula.problems.leetcode.learn.bst;
+
+import java.util.Stack;
+
+/**
+ * №98 Validate Binary Search Tree
+ *
+ * Given the root of a binary tree, determine if it is a valid binary search tree (BST).
+ * A valid BST is defined as follows:
+ * - The left subtree of a node contains only nodes with keys less than the node's key.
+ * - The right subtree of a node contains only nodes with keys greater than the node's key.
+ * - Both the left and right subtrees must also be binary search trees.
+ */
+public class ValidateBinarySearchTree {
+
+    public static void main(String... args) {
+
+        var leaf1 = new TreeNode(1);
+        var leaf2 = new TreeNode(3);
+        var root = new TreeNode(2, leaf1, leaf2);
+
+        System.out.println(
+            isValidBST(root)
+        );
+    }
+
+    private static boolean isValidBST(TreeNode root) {
+
+        if (root == null) {
+            return true;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode previous = null;
+
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (previous != null && root.val <= previous.val) {
+                return false;
+            }
+            previous = root;
+            root = root.right;
+        }
+
+        return true;
+    }
+
+    private static class TreeNode {
+
+        private final int val;
+
+        private TreeNode left;
+        private TreeNode right;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
