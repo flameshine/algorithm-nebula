46e53da74f8e52e914bed1b1f9da81dbeb8f369e
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2024-06-23 12:31:44 +0200

    Time: 2 ms (71.98%), Space: 61.9 MB (19.24%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        var result = 0;
+        var cheapest = prices[0];
+
+        for (var price : prices) {
+            result = Math.max(result, price - cheapest);
+            cheapest = Math.min(cheapest, price);
+        }
+
+        return result;
+    }
+}
