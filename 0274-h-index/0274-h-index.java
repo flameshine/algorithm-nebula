4cdd0f3a4571555a5544a6b404c7c2cf0e3d7192
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2024-06-24 17:58:30 +0200

    Time: 2 ms (68.86%), Space: 41 MB (92.52%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    public int hIndex(int[] citations) {
+
+        Arrays.sort(citations);
+
+        for (var i = 0; i < citations.length; i++) {
+            if (citations[i] >= citations.length - i) {
+                return citations.length - i;
+            }
+        }
+
+        return 0;
+    }
+}
