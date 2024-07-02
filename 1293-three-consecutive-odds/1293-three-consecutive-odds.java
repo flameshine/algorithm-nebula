ae36f94416039bc147140a7598f9acf7737fdf83
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2024-07-02 19:54:32 +0200

    Time: 0 ms (100%), Space: 41.9 MB (86.94%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.java
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.java
@@ -1,5 +1,23 @@
 class Solution {
+
     public boolean threeConsecutiveOdds(int[] arr) {
         
+        var counter = 0;
+
+        for (var n : arr) {
+
+            if (counter == 3) {
+                return true;
+            }
+
+            if (n % 2 == 0) {
+                counter = 0;
+                continue;
+            }
+
+            ++counter;
+        }
+
+        return counter == 3;
     }
 }
