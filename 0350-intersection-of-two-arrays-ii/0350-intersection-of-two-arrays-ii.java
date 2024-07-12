402df1a3a67b35154bbe44a0facc5b2431c3b6d8
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2024-07-12 17:02:16 +0200

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
+++ 0350-intersection-of-two-arrays-ii/0350-intersection-of-two-arrays-ii.java
@@ -1,28 +1,5 @@
 class Solution {
-    
     public int[] intersect(int[] nums1, int[] nums2) {
-
-        Map<Integer, Integer> numberToCount1 = new HashMap<>();
-
-        for (var n : nums1) {
-            numberToCount1.merge(n, 1, Integer::sum);
-        }
-
-        List<Integer> resultBuilder = new ArrayList<>();
-
-        for (var n : nums2) {
-            if (numberToCount1.containsKey(n) && numberToCount1.get(n) > 0) {
-                resultBuilder.add(n);
-                numberToCount1.merge(n, -1, Integer::sum);
-            }
-        }
-
-        var result = new int[resultBuilder.size()];
-
-        for (var i = 0; i < resultBuilder.size(); i++) {
-            result[i] = resultBuilder.get(i);
-        }
-
-        return result;
+        
     }
 }
