2d43d7ff12cc24591a40fbe8d650787347f12a47
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2024-07-12 17:04:30 +0200

    Time: 2 ms (86.19%), Space: 55.1 MB (81.94%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,51 @@
+class Solution {
+    
+    public int[] productExceptSelf(int[] nums) {
+        
+        var zeroes = 0;
+
+        for (var n : nums) {
+            if (n == 0) {
+                ++zeroes;
+            }
+        }
+
+        var result = new int[nums.length];
+
+        return switch (zeroes) {
+            case 0 -> {
+                
+                var product = 1;
+
+                for (var n : nums) {
+                    product *= n;
+                }
+
+                for (var i = 0; i < nums.length; i++) {
+                    result[i] = product / nums[i];
+                }
+
+                yield result;
+            }
+            case 1 -> {
+
+                var productExceptZero = 1;
+
+                for (var n : nums) {
+                    if (n != 0) {
+                        productExceptZero *= n;
+                    }
+                }
+
+                for (var i = 0; i < nums.length; i++) {
+                    if (nums[i] == 0) {
+                        result[i] = productExceptZero;
+                    }
+                }
+
+                yield result;
+            }
+            default -> result;
+        };
+    }
+}
