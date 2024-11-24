1f52809e333cd30b5583900c6c229a5c9c071f5b
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2024-11-24 12:18:48 +0100

    Time: 0 ms (100%), Space: 42.2 MB (29.87%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,16 +1,16 @@
 class Solution {
-    
+
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
         var nums1Pointer = m - 1;
         var nums2Pointer = n - 1;
-        var resultPointer = m + n - 1;
+        var i = m + n - 1;
 
         while (nums2Pointer >= 0) {
             if (nums1Pointer >= 0 && nums1[nums1Pointer] > nums2[nums2Pointer]) {
-                nums1[resultPointer--] = nums1[nums1Pointer--];
+                nums1[i--] = nums1[nums1Pointer--];
             } else {
-                nums1[resultPointer--] = nums2[nums2Pointer--];
+                nums1[i--] = nums2[nums2Pointer--];
             }
         }
     }
