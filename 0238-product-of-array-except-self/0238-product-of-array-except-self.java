f2a6bc0e0ba78326557f26dd2e732db556b247b2
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-01-23 15:33:28 +0100

    Time: 3 ms (23.81%), Space: 55.3 MB (74.44%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -2,50 +2,50 @@ class Solution {
     
     public int[] productExceptSelf(int[] nums) {
         
-        var zeroes = 0;
+        var zeros = 0;
 
         for (var n : nums) {
             if (n == 0) {
-                ++zeroes;
+                ++zeros;
             }
+        }
+        
+        if (zeros > 1) {
+            return new int[nums.length];
+        }
+
+        if (zeros == 1) {
+
+            var product = 1;
+            var index = 0;
+
+            for (var i = 0; i < nums.length; i++) {
+                if (nums[i] != 0) {
+                    product *= nums[i];
+                } else {
+                    index = i;
+                }
+            }
+
+            var result = new int[nums.length];
+
+            result[index] = product;
+
+            return result;
+        }
+
+        var product = 1;
+
+        for (var n : nums) {
+            product *= n;
         }
 
         var result = new int[nums.length];
 
-        return switch (zeroes) {
-            case 0 -> {
-                
-                var product = 1;
+        for (var i = 0; i < nums.length; i++) {
+            result[i] = product / nums[i];
+        }
 
-                for (var n : nums) {
-                    product *= n;
-                }
-
-                for (var i = 0; i < nums.length; i++) {
-                    result[i] = product / nums[i];
-                }
-
-                yield result;
-            }
-            case 1 -> {
-
-                var productExceptZero = 1;
-
-                for (var n : nums) {
-                    if (n != 0) {
-                        productExceptZero *= n;
-                    }
-                }
-
-                for (var i = 0; i < nums.length; i++) {
-                    if (nums[i] == 0) {
-                        result[i] = productExceptZero;
-                    }
-                }
-
-                yield result;
-            }
-            default -> result;
-        };
+        return result;
     }
 }
