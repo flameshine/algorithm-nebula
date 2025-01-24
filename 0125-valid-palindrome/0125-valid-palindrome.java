039cbed4c77f5bd4bc2b23951a5d22848797ea88
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-01-24 11:21:29 +0100

    Time: 2 ms (99.1%), Space: 42.9 MB (89.28%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    public boolean isPalindrome(String s) {
+        
+        var left = 0;
+        var right = s.length() - 1;
+
+        while (left < right) {
+
+            var leftChar = s.charAt(left);
+            var rightChar = s.charAt(right);
+
+            if (!Character.isLetterOrDigit(leftChar)) {
+                ++left;
+            } else if (!Character.isLetterOrDigit(rightChar)) {
+                --right;
+            } else {
+
+                if (Character.toLowerCase(leftChar) != Character.toLowerCase(rightChar)) {
+                    return false;
+                }
+
+                ++left;
+                --right;
+            }
+        }
+
+        return true;
+    }
+}
