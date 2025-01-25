5d3a686312bdd5e5159de57d93ec059911870af6
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-01-25 21:15:16 +0100

    Time: 513 ms (19.24%), Space: 52.3 MB (28.45%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,29 +1,32 @@
 class Solution {
-    
+
     public List<List<Integer>> threeSum(int[] nums) {
         
         if (nums.length == 0) {
             return List.of();
         }
 
+        Arrays.sort(nums);
+        
         Set<List<Integer>> resultBuilder = new HashSet<>();
 
         for (var i = 0; i < nums.length; i++) {
 
-            Set<Integer> unique = new HashSet<>();
+            var j = i + 1;
+            var k = nums.length - 1;
 
-            for (var j = i + 1; j < nums.length; j++) {
+            while (j < k) {
 
-                var missing = -nums[i] - nums[j];
+                var sum = nums[i] + nums[j] + nums[k];
 
-                if (unique.contains(missing)) {
-                    var triple = List.of(nums[i], nums[j], missing).stream()
-                        .sorted()
-                        .toList();
+                if (sum == 0) {
+                    var triple = List.of(nums[i], nums[j++], nums[k--]);
                     resultBuilder.add(triple);
+                } else if (sum < 0) {
+                    ++j;
+                } else {
+                    --k;
                 }
-
-                unique.add(nums[j]);
             }
         }
 
