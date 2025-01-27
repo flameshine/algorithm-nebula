5dea6e231343a4317f24c5ac8c466e8805914559
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-01-27 19:29:21 +0100

    Time: 10 ms (74.27%), Space: 45 MB (42.89%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -2,25 +2,25 @@ class Solution {
 
     public int characterReplacement(String s, int k) {
         
-        var counts = new int[26];
-        var maxCount = 0;
+        var frequencies = new int[26];
+        var maxFrequency = 0;
         var start = 0;
         var result = 0;
 
-        for (var i = 0; i < s.length(); i++) {
+        for (var end = 0; end < s.length(); end++) {
 
-            var current = s.charAt(i);
+            var current = s.charAt(end);
+            
+            ++frequencies[current - 'A'];
 
-            ++counts[current - 'A'];
+            maxFrequency = Math.max(maxFrequency, frequencies[current - 'A']);
 
-            maxCount = Math.max(maxCount, counts[current - 'A']);
-
-            if (i - start + 1 - maxCount > k) {
-                counts[s.charAt(start) - 'A']--;
+            if (end - start + 1 - maxFrequency > k) {
+                --frequencies[s.charAt(start) - 'A'];
                 ++start;
             }
 
-            result = Math.max(result, i - start + 1);
+            result = Math.max(result, end - start + 1);
         }
 
         return result;
