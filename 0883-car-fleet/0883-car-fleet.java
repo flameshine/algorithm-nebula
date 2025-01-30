1cc873d36f0657c25a72497945a9d4a1ea610fe5
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-01-30 23:45:58 +0100

    Time: 94 ms (26.92%), Space: 56.4 MB (72.23%) - LeetHub

---
+++ 0883-car-fleet/0883-car-fleet.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    public int carFleet(int target, int[] position, int[] speed) {
+
+        Queue<Car> heap = new PriorityQueue<>((a, b) -> b.position - a.position);
+
+        for (var i = 0; i < position.length; i++) {
+            heap.add(new Car(position[i], speed[i]));
+        }
+
+        double slowest = 0;
+        var result = 0;
+
+        while (!heap.isEmpty()) {
+
+            var car = heap.poll();
+            var time = (double) (target - car.position) / car.speed;
+
+            if (slowest < time) {
+                slowest = time;
+                ++result;
+            }
+        }
+
+        return result;
+    }
+
+    private static class Car {
+
+        private final int position;
+        private final int speed;
+
+        public Car(int position, int speed) {
+            this.position = position;
+            this.speed = speed;
+        }
+    }
+}
