db65a64f04e7397c4295cb709aafba56c44785d8
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-01-31 22:33:12 +0100

    Time: 27 ms (23.57%), Space: 45.3 MB (46.34%) - LeetHub

---
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -0,0 +1,34 @@
+class Solution {
+
+    public int minEatingSpeed(int[] piles, int h) {
+        
+        var max = Integer.MIN_VALUE;
+
+        for (var p : piles) {
+            max = Math.max(max, p);
+        }
+
+        var left = 1;
+        var right = max;
+        var result = right;
+
+        while (left <= right) {
+
+            var middle = left + (right - left) / 2;
+            var hours = 0;
+
+            for (var p : piles) {
+                hours += Math.ceil((double) p / middle);
+            }
+
+            if (hours <= h) {
+                result = Math.min(result, middle);
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        return result;
+    }
+}
