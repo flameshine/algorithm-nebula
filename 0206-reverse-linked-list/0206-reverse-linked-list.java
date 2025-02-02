9926763d0f31933329534368ba2645b590efda3a
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-02-02 14:17:25 +0100

    Time: 0 ms (100%), Space: 42.5 MB (66.62%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    public ListNode reverseList(ListNode head) {
+        
+        ListNode result = null;
+
+        while (head != null) {
+            
+            if (result == null) {
+                result = new ListNode(head.val);
+            } else {
+                result = new ListNode(head.val, result);
+            }
+
+            head = head.next;
+        }
+
+        return result;
+    }
+}
