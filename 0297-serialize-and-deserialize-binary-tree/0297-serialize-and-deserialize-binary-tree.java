4e87f1035b891ffd838005e8041257192f6b8316
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-02-11 18:16:49 +0100

    Time: 7 ms (94.7%), Space: 46.1 MB (47.37%) - LeetHub

---
+++ 0297-serialize-and-deserialize-binary-tree/0297-serialize-and-deserialize-binary-tree.java
@@ -0,0 +1,46 @@
+public class Codec {
+
+    public String serialize(TreeNode root) {
+        var builder = new StringBuilder();
+        serialize(root, builder);
+        return builder.toString();
+    }
+
+
+    public TreeNode deserialize(String data) {
+        var index = new AtomicInteger();
+        var tokens = data.split(",");
+        return deserialize(tokens, index);
+    }
+
+    private static void serialize(TreeNode root, StringBuilder builder) {
+
+        if (root == null) {
+            builder.append("null")
+                .append(",");
+            return;
+        }
+
+        builder.append(root.val)
+            .append(",");
+
+        serialize(root.left, builder);
+        serialize(root.right, builder);
+    }
+
+    private static TreeNode deserialize(String[] tokens, AtomicInteger index) {
+
+        if (index.get() >= tokens.length || tokens[index.get()].equals("null")) {
+            index.incrementAndGet();
+            return null;
+        }
+
+        var value = Integer.parseInt(tokens[index.getAndIncrement()]);
+        var root = new TreeNode(value);
+
+        root.left = deserialize(tokens, index);
+        root.right= deserialize(tokens, index);
+
+        return root;
+    }
+}
