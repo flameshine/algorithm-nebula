bc947b36cfab68aaa14f7f7f125ce793d39ae19a
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-03-03 23:18:50 +0100

    Time: 1 ms (83.22%), Space: 42 MB (36.61%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -2,26 +2,20 @@ class Solution {
 
     public String mergeAlternately(String word1, String word2) {
         
-        var word1Index = 0;
-        var word2Index = 0;
-        var flag = true;
         var result = new StringBuilder();
+        var i = 0;
 
-        while (word1Index < word1.length() && word2Index < word2.length()) {
+        while (i < word1.length() || i < word2.length()) {
 
-            if (flag) {
-                result.append(word1.charAt(word1Index++));
-            } else {
-                result.append(word2.charAt(word2Index++));
+            if (i < word1.length()) {
+                result.append(word1.charAt(i));
+            }
+            
+            if (i < word2.length()) {
+                result.append(word2.charAt(i));
             }
 
-            flag = !flag;
-        }
-
-        if (word1Index == word1.length()) {
-            result.append(word2.substring(word2Index));
-        } else if (word2Index == word2.length()) {
-            result.append(word1.substring(word1Index));
+            ++i;
         }
 
         return result.toString();
