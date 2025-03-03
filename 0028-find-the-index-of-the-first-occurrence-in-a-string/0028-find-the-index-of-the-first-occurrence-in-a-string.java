f234e01219e9dae88fea5dbca652ad98190f403f
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-03-03 22:43:05 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -3,14 +3,7 @@ class Solution {
     public int strStr(String haystack, String needle) {
         
         for (var i = 0; i < haystack.length() - needle.length() + 1; i++) {
-
-            var builder = new StringBuilder();
-
-            for (var j = i; j < i + needle.length(); j++) {
-                builder.append(haystack.charAt(j));
-            }
-
-            if (builder.toString().equals(needle)) {
+            if (haystack.startsWith(needle)) {
                 return i;
             }
         }
