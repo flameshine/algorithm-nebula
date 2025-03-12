e044eaf1891df1bc39e89ed3949fd3e4cea99721
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-03-12 21:55:00 +0100

    Time: 1 ms (99.66%), Space: 44.3 MB (31.64%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.java
@@ -0,0 +1,31 @@
+class Solution {
+
+    public int compress(char[] chars) {
+
+        var result = 0;
+        var i = 0;
+    
+        while (i < chars.length) {
+
+            var current = chars[i];
+            var count = 0;
+
+            while (i < chars.length && chars[i] == current) {
+                ++count;
+                ++i;
+            }
+
+            chars[result++] = current;
+
+            if (count <= 1) {
+                continue;
+            }
+
+            for (var c : String.valueOf(count).toCharArray()) {
+                chars[result++] = c;
+            }
+        }
+
+        return result;
+    }
+}
