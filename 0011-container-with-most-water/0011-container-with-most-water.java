82e0123269c9264e29bc83c88178693f99025248
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-03-14 10:47:10 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,20 +1,16 @@
 class Solution {
 
     public int maxArea(int[] heights) {
-             
-        if (heights.length == 0) {
-            return 0;
-        }
-
-        var result = 0;
+        
         var left = 0;
         var right = heights.length - 1;
+        var result = 0;
 
         while (left < right) {
 
-            var current = Math.min(heights[left], heights[right]) * (right - left);
-
-            result = Math.max(result, current);
+            var area = Math.min(heights[left], heights[right] * (right - left));
+            
+            result = Math.max(result, area);
 
             if (heights[left] < heights[right]) {
                 ++left;
