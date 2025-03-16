821c0720c48db2393175cefaaf5449608f88e3e6
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-03-16 17:25:00 +0100

    Time: 28 ms (69.77%), Space: 46.2 MB (99.98%) - LeetHub

--- 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/2428-equal-row-and-column-pairs.java
@@ -2,32 +2,23 @@ class Solution {
 
     public int equalPairs(int[][] grid) {
 
-        List<String> rows = new ArrayList<>();
-        List<String> columns = new ArrayList<>();
-
-        for (var i = 0; i < grid.length; i++) {
-
-            var row = new StringBuilder();
-            var column = new StringBuilder();
-
-            for (var j = 0; j < grid[0].length; j++) {
-                
-                row.append(grid[i][j])
-                    .append("-");
-
-                column.append(grid[j][i])
-                    .append("-");
-            }
-
-            rows.add(row.toString());
-            columns.add(column.toString());
-        }
-
+        var n = grid.length;
         var result = 0;
 
-        for (var row : rows) {
-            for (var column : columns) {
-                if (row.equals(column)) {
+        for (var i = 0; i < n; i++) {
+
+            for (var j = 0; j < n; j++) {
+
+                var isEqual = true;
+
+                for (var x = 0; x < n; x++) {
+                    if (grid[i][x] != grid[x][j]) {
+                        isEqual = false;
+                        break;
+                    }
+                }
+
+                if (isEqual) {
                     ++result;
                 }
             }
