e6cfaf5117efdf8b4ddd6d31a8222b09c18d4c3b
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-03-18 10:46:40 +0100

    Time: 25 ms (13.97%), Space: 56.2 MB (5.33%) - LeetHub

---
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.java
@@ -0,0 +1,19 @@
+class RecentCounter {
+
+    private final Queue<Integer> heap;
+
+    public RecentCounter() {
+        this.heap = new PriorityQueue<>();
+    }
+    
+    public int ping(int t) {
+
+        while (!heap.isEmpty() && heap.peek() < t - 3000) {
+            heap.poll();
+        }
+        
+        heap.add(t);
+
+        return heap.size();
+    }
+}
