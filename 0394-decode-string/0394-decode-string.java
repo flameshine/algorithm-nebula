633617e8229ad4e341255ada66387a870db6633f
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-03-18 10:23:58 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.java
@@ -0,0 +1,15 @@
+class Solution {
+    
+    public String decodeString(String s) {
+
+        Stack<String> fragments = new Stack<>();
+        Stack<Integer> counts = new Stack<>();
+
+        var builder = new StringBuilder();
+        var number = 0;
+
+        for (var c : s.toCharArray()) {
+            
+        }
+    }
+}
