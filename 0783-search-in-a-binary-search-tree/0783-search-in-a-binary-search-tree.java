4593efb112e885063d4e3bf9e8c1d0e5e43ce03b
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-03-19 23:00:08 +0100

    Time: 0 ms (100%), Space: 45.1 MB (85.87%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.java
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.java
@@ -4,14 +4,16 @@ class Solution {
         
         var pointer = root;
 
-        while (pointer != null && pointer.val != val) {
+        while (pointer != null) {
             if (pointer.val > val) {
                 pointer = pointer.left;
-            } else {
+            } else if (pointer.val < val) {
                 pointer = pointer.right;
+            } else {
+                return pointer;
             }
         }
 
-        return pointer;
+        return null;
     }
 }
