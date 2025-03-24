e1fb94ee6770e4ef30834c7a8b56f208bb96ec2c
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-03-24 14:28:13 +0100

    Time: 2 ms (98.83%), Space: 45.2 MB (17.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,14 +1,17 @@
 class Solution {
-    
-    public int[] twoSum(int[] nums, int target) {
-                
-        Map<Integer, Integer> numberToIndex = new HashMap<>();
 
+    public int[] twoSum(int[] nums, int target) {
+
+        Map<Integer, Integer> numberToIndex = new HashMap<>();
+        
         for (var i = 0; i < nums.length; i++) {
+
             var required = target - nums[i];
+
             if (numberToIndex.containsKey(required)) {
-                return new int[] { numberToIndex.get(required), i };
+                return new int[] { i, numberToIndex.get(required) };
             }
+
             numberToIndex.put(nums[i], i);
         }
 
