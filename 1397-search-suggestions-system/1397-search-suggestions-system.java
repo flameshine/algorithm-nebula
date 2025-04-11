445125481f862ab21db6ba8382a0f2e005685d77
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-04-11 22:29:46 +0200

    Time: 19 ms (68.25%), Space: 48.9 MB (35.69%) - LeetHub

--- 1397-search-suggestions-system/1397-search-suggestions-system.java
+++ 1397-search-suggestions-system/1397-search-suggestions-system.java
@@ -43,31 +43,31 @@ class Solution {
     private TrieNode[] children;
     private PriorityQueue<String> pq;
     
-    TrieNode() {
-        this.children = new TrieNode[26];
-        this.pq = new PriorityQueue<>((a,b) -> b.compareTo(a));
-    }
+        TrieNode() {
+            this.children = new TrieNode[26];
+            this.pq = new PriorityQueue<>((a,b) -> b.compareTo(a));
+        }
     
-    public void addToPQ(String word) {
+        public void addToPQ(String word) {
 
-        pq.add(word);
+            pq.add(word);
 
-        if (pq.size() > 3) {
-            pq.poll();
+            if (pq.size() > 3) {
+                pq.poll();
+            }
+        }
+    
+        public List<String> getTopThree() {
+
+            List<String> topThree = new ArrayList<>();
+
+            while (!pq.isEmpty()) {
+                topThree.add(pq.poll());
+            }
+
+            Collections.reverse(topThree);
+
+            return topThree;
         }
     }
-    
-    public List<String> getTopThree() {
-
-        List<String> topThree = new ArrayList<>();
-
-        while (!pq.isEmpty()) {
-            topThree.add(pq.poll());
-        }
-
-        Collections.reverse(topThree);
-
-        return topThree;
-    }
-}
 }
