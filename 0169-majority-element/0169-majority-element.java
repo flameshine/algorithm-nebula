4648436608e782adc1d9a4744de03dad8cb92c22
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-04-19 10:34:02 +0200

    Time: 13 ms (29.31%), Space: 50.2 MB (71.85%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -5,10 +5,9 @@ class Solution {
         Map<Integer, Integer> frequencies = new HashMap<>();
 
         for (var n : nums) {
-            frequencies.merge(n, 1, Integer::sum);
-        }
 
-        for (var n : nums) {
+            frequencies.merge(n, 1, Integer::sum);
+
             if (frequencies.get(n) > nums.length / 2) {
                 return n;
             }
