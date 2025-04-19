ca6853213309dc718623b184b785bb4022d73d7e
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-04-19 10:33:43 +0200

    Time: 9 ms (36.32%), Space: 50 MB (72.58%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,15 +1,17 @@
 class Solution {
-    
+
     public int majorityElement(int[] nums) {
-        
+
         Map<Integer, Integer> frequencies = new HashMap<>();
 
         for (var n : nums) {
-            var frequency = frequencies.getOrDefault(n, 0);
-            if (frequency == nums.length / 2) {
+            frequencies.merge(n, 1, Integer::sum);
+        }
+
+        for (var n : nums) {
+            if (frequencies.get(n) > nums.length / 2) {
                 return n;
             }
-            frequencies.put(n, frequency + 1);
         }
 
         throw new IllegalStateException();
