87ce1f0bfcc7c972643106a537f7618e469531b7
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-04-19 10:34:11 +0200

    Time: 13 ms (29.31%), Space: 49.9 MB (73.68%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -7,7 +7,7 @@ class Solution {
         for (var n : nums) {
 
             frequencies.merge(n, 1, Integer::sum);
-
+            
             if (frequencies.get(n) > nums.length / 2) {
                 return n;
             }
