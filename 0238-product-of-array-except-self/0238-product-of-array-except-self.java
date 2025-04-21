3648353453a40038017a1a43a5eb3c6e3c7f8fac
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-04-21 11:50:17 +0200

    Time: 3 ms (21.32%), Space: 56 MB (29.33%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -2,26 +2,22 @@ class Solution {
 
     public int[] productExceptSelf(int[] nums) {
         
-        var prefix = new int[nums.length];
-
-        prefix[0] = 1;
-
-        for (var i = 1; i < nums.length; i++) {
-            prefix[i] = nums[i - 1] * prefix[i - 1];
-        }
-
-        var suffix = new int[nums.length];
-
-        suffix[nums.length - 1] = 1;
-
-        for (var i = nums.length - 2; i >= 0; i--) {
-            suffix[i] = nums[i + 1] * suffix[i + 1];
-        }
-
         var result = new int[nums.length];
 
+        Arrays.fill(result, 1);
+
+        var current = 1;
+
         for (var i = 0; i < nums.length; i++) {
-            result[i] = prefix[i] * suffix[i];
+            result[i] *= current;
+            current *= nums[i];
+        }
+
+        current = 1;
+
+        for (var i = nums.length - 1; i >= 0; i--) {
+            result[i] *= current;
+            current *= nums[i];
         }
 
         return result;
