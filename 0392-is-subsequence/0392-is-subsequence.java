00234a5d4f074b40e637c4664a4ba8142c8ce3c0
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-04-26 22:53:13 +0200

    Time: 1 ms (93.62%), Space: 42 MB (9.6%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,15 +1,11 @@
 class Solution {
 
     public boolean isSubsequence(String s, String t) {
-
-        if (s.isBlank() || t.isBlank()) {
-            return false;
-        }
         
         var i = 0;
 
         for (var c : t.toCharArray()) {
-            if (s.charAt(i) == c) {
+            if (i < s.length() && s.charAt(i) == c) {
                 ++i;
             }
         }
