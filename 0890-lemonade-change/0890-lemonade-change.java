56a7b96d5c58d9028b27b3e7d0a20da630de4320
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-05-04 11:55:35 +0200

    Time: 2 ms (94.08%), Space: 56.5 MB (12.2%) - LeetHub

---
+++ 0890-lemonade-change/0890-lemonade-change.java
@@ -0,0 +1,38 @@
+class Solution {
+
+    public boolean lemonadeChange(int[] bills) {
+        
+        var balance5 = 0;
+        var balance10 = 0;
+        var balance20 = 0;
+
+        for (var b : bills) {
+            switch (b) {
+                case 5 -> ++balance5;
+                case 10 -> {
+                    if (balance5 > 0) {
+                        ++balance10;
+                        --balance5;
+                    } else {
+                        return false;
+                    }
+                }
+                case 20 -> {
+
+                    if (balance5 > 0 && balance10 > 0) {
+                        --balance5;
+                        --balance10;
+                    } else if (balance5 > 2) {
+                        balance5 -= 3;
+                    } else {
+                        return false;
+                    }
+
+                    ++balance20;
+                }
+            }
+        }
+
+        return true;
+    }
+}
