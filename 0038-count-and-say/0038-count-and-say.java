de9e0cb55689a616f83859fd753fa88c21597c34
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-05-09 21:16:57 +0200

    Time: 2 ms (91.16%), Space: 41.2 MB (84.64%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -1,27 +1,32 @@
 class Solution {
-
+    
     public String countAndSay(int n) {
-        
+
         if (n == 1) {
             return "1";
         }
 
         var s = countAndSay(n - 1);
         var builder = new StringBuilder();
+        var count = 1;
+        var current = s.charAt(0);
 
-        for (var i = 0; i < s.length(); i++) {
-
-            var count = 1;
-            var c = s.charAt(i);
-
-            while (i < s.length() - 1 && s.charAt(i) == s.charAt(i + 1)) {
+        for (var i = 1; i < s.length(); i++) {
+            if (s.charAt(i) == current) {
                 ++count;
-                ++i;
-            }
+            } else {
 
-            builder.append(count)
-                .append(c);
+                builder.append(count)
+                    .append(current);
+
+                current = s.charAt(i);
+
+                count = 1;
+            }
         }
+
+        builder.append(count)
+            .append(current);
 
         return builder.toString();
     }
