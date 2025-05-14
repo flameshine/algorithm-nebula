415193ab5d2d243c1559c360ff13601abc5aa69c
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-05-14 19:16:44 +0200

    Time: 593 ms (28.5%), Space: 41.4 MB (80.03%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,22 +1,22 @@
 class Solution {
 
     public int findTargetSumWays(int[] nums, int target) {
-        return findTargetSumWays(nums, target, nums.length - 1, 0);
+        return findTargetSumWays(nums, target, 0, 0);
     }
 
     private static int findTargetSumWays(int[] nums, int target, int i, int sum) {
 
-        if (i < 0 && sum == target) {
+        if (i >= nums.length && sum == target) {
             return 1;
         }
 
-        if (i < 0) {
+        if (i >= nums.length) {
             return 0;
         }
 
-        var add = findTargetSumWays(nums, target, i - 1, sum + nums[i]);
-        var substruct = findTargetSumWays(nums, target, i - 1, sum - nums[i]);
+        var negative = findTargetSumWays(nums, target, i + 1, sum - nums[i]);
+        var positive = findTargetSumWays(nums, target, i + 1, sum + nums[i]);
 
-        return add + substruct;
+        return negative + positive;
     }
 }
