7c9bca730d07c91db66c1cd9f5707df669b06193
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-05-15 22:25:33 +0200

    Time: 111 ms (7.59%), Space: 46.6 MB (5.81%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,17 +2,18 @@ class Solution {
 
     public int coinChange(int[] coins, int amount) {
         Map<Integer, Integer> memory = new HashMap<>();
-        return coinChange(coins, amount, memory);
+        var result = coinChange(coins, amount, memory);
+        return result == Integer.MAX_VALUE ? -1 : result;
     }
 
     private static int coinChange(int[] coins, int amount, Map<Integer, Integer> memory) {
 
-        if (amount < 0) {
-            return -1;
-        }
-
         if (amount == 0) {
             return 0;
+        }
+
+        if (amount < 0) {
+            return Integer.MAX_VALUE;
         }
 
         var previouslyComputedChange = memory.get(amount);
@@ -21,14 +22,14 @@ class Solution {
             return previouslyComputedChange;
         }
 
-        var result = -1;
+        var result = Integer.MAX_VALUE;
 
         for (var i = 0; i < coins.length; i++) {
 
             var change = coinChange(coins, amount - coins[i], memory);
             
-            if (change >= 0) {
-                result = result < 0 ? change + 1 : Math.min(result, change + 1);
+            if (change != Integer.MAX_VALUE) {
+                result = Math.min(result, change + 1);
             }
         }
 
