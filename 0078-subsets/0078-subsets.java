e8c73a632aa437ede72b36241004351adc4df9d5
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-05-19 14:00:21 +0200

    Time: 0 ms (100%), Space: 42.3 MB (99.21%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,12 +1,12 @@
 class Solution {
-
+    
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        createSubset(result, new ArrayList<>(), nums, 0);
+        backtrack(result, new ArrayList<>(), nums, 0);
         return result;
     }
 
-    private static void createSubset(
+    private static void backtrack(
         List<List<Integer>> result,
         List<Integer> subset,
         int[] nums,
@@ -19,8 +19,8 @@ class Solution {
         }
 
         subset.add(nums[index]);
-        createSubset(result, subset, nums, index + 1);
+        backtrack(result, subset, nums, index + 1);
         subset.remove(subset.size() - 1);
-        createSubset(result, subset, nums, index + 1);
-    }
+        backtrack(result, subset, nums, index + 1);
+    } 
 }
