7e8a21517866822ae51187a59151b2838a8c920c
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-05-24 15:08:56 +0200

    Time: 1 ms (15.12%), Space: 44.9 MB (54.44%) - LeetHub

--- 1382-calculate-amount-paid-in-taxes/1382-calculate-amount-paid-in-taxes.java
+++ 1382-calculate-amount-paid-in-taxes/1382-calculate-amount-paid-in-taxes.java
@@ -3,22 +3,21 @@ class Solution {
     public double calculateTax(int[][] brackets, int income) {
         
         double taxesPaid = 0;
-        var remainingTaxableIncome = income;
         var taxedValue = 0;
         
         for (var bracket : brackets) {
 
             var taxableAmount = 0;
 
-            if (remainingTaxableIncome > bracket[0] - taxedValue) {
+            if (income > bracket[0] - taxedValue) {
                 taxableAmount = bracket[0] - taxedValue;
             } else {
-                taxableAmount = remainingTaxableIncome;
+                taxableAmount = income;
             }
 
             taxesPaid += taxableAmount * bracket[1] / 100D;
             taxedValue += taxableAmount;
-            remainingTaxableIncome -= taxableAmount;
+            income -= taxableAmount;
         }
 
         return taxesPaid;
