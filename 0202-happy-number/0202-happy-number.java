84c6cd191a478a2c2711f1cd82a1e22038b9171d
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-05-24 19:04:57 +0200

    Time: 1 ms (67.58%), Space: 40.7 MB (66.11%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -10,27 +10,18 @@ class Solution {
             return true;
         }
 
-        if (seen.contains(n)) {
+        if (!seen.add(n)) {
             return false;
         }
 
-        seen.add(n);
-
-        var sum = sumOfSquaredDigits(n);
-
-        return isHappy(sum, seen);
-    }
-
-    private static int sumOfSquaredDigits(int n) {
-
-        var sum = 0;
+        var sumOfSquaredDigits = 0;
 
         while (n != 0) {
             var digit = n % 10;
-            sum += digit * digit;
+            sumOfSquaredDigits += Math.pow(digit, 2);
             n /= 10;
         }
 
-        return sum;
+        return isHappy(sumOfSquaredDigits, seen);
     }
 }
