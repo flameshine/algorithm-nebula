7d458f6f4203bc05103005ff66c96ff86c522215
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-05-26 11:47:09 +0200

    Time: 8 ms (73.49%), Space: 44.6 MB (36.79%) - LeetHub

--- 0224-basic-calculator/0224-basic-calculator.java
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -1,41 +1,30 @@
 class Solution {
     
     public int calculate(String s) {
-        
-        Stack<Integer> stack = new Stack<>();
 
         var result = 0;
         var number = 0;
         var sign = 1;
 
-        for (var c : s.toCharArray()) {
+        Stack<Integer> stack = new Stack<>();
 
+        stack.push(sign);
+
+        for (var c : s.toCharArray()) {
             if (Character.isDigit(c)) {
-                number = 10 * number + (int) (c - '0');
-            } else if (c == '+') {
+                number = number * 10 + (c - '0');
+            } else if (c == '+' || c == '-') {
                 result += sign * number;
                 number = 0;
-                sign = 1;
-            } else if (c == '-') {
-                result += sign * number;
-                number = 0;
-                sign = -1;
+                sign = stack.peek() * (c == '+' ? 1 : -1);
             } else if (c == '(') {
-                stack.push(result);
                 stack.push(sign);
-                sign = 1;
-                result = 0;
             } else if (c == ')') {
-                result += sign * number;
-                number = 0;
-                result *= stack.pop();
-                result += stack.pop();
+                stack.pop();
             }
         }
 
-        if (number != 0) {
-            result += sign * number;
-        }
+        result += sign * number;
 
         return result;
     }
