ca3eb54d0cac3fd69bbfdc53e42c6cb14ba96d18
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-05-28 22:09:26 +0200

    Time: 8 ms (40.6%), Space: 48 MB (45.51%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,21 +1,19 @@
 class Solution {
-    
+
     public List<List<String>> groupAnagrams(String[] strs) {
         
         Map<Integer, List<String>> stringsByHashes = new HashMap<>();
 
         for (var s : strs) {
-            var hash = buildHash(s);
-            var strings = stringsByHashes.getOrDefault(hash, new ArrayList<>());
-            strings.add(s);
-            stringsByHashes.put(hash, strings);
+            stringsByHashes.computeIfAbsent(hash(s), k -> new ArrayList<>())
+                .add(s);
         }
 
         return stringsByHashes.values().stream()
             .toList();
     }
 
-    private static int buildHash(String s) {
+    private static Integer hash(String s) {
         var chars = s.toCharArray();
         Arrays.sort(chars);
         return Arrays.hashCode(chars);
