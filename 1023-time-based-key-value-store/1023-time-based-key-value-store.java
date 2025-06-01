7e0e92340b82db6921b4e45ba45887ca1d652d61
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-06-01 11:09:30 +0200

    Time: 130 ms (94.51%), Space: 118.4 MB (85.08%) - LeetHub

--- 1023-time-based-key-value-store/1023-time-based-key-value-store.java
+++ 1023-time-based-key-value-store/1023-time-based-key-value-store.java
@@ -1,6 +1,6 @@
 class TimeMap {
 
-    private final Map<String, List<Data>> map;
+    private final Map<String, List<Entry>> map;
 
     public TimeMap() {
         this.map = new HashMap<>();
@@ -8,7 +8,7 @@ class TimeMap {
     
     public void set(String key, String value, int timestamp) {
         map.computeIfAbsent(key, k -> new ArrayList<>())
-            .add(new Data(value, timestamp));
+            .add(new Entry(value, timestamp));
     }
     
     public String get(String key, int timestamp) {
@@ -17,23 +17,23 @@ class TimeMap {
             return "";
         }
 
-        var data = map.get(key);
+        var entries = map.get(key);
         var left = 0;
-        var right = data.size() - 1;
+        var right = entries.size() - 1;
 
         while (left < right) {
             var middle = (left + right + 1) >>> 1;
-            if (timestamp < data.get(middle).timestamp()) {                 
+            if (timestamp < entries.get(middle).timestamp()) {                 
                 right = middle - 1;
             } else {
                 left = middle;
             }
         }
 
-        var item = data.get(left);
+        var item = entries.get(left);
 
         return item.timestamp() <= timestamp ? item.value() : "";
     }
 
-    private record Data(String value, int timestamp) {}
+    private record Entry(String value, int timestamp) {}
 }
