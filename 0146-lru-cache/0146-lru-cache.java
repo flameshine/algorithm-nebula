601f73bf24f2719024e113d2bc8b41ce44d7eeb7
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-06-01 12:27:59 +0200

    Time: 56 ms (14.87%), Space: 115.1 MB (46.82%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,21 +1,72 @@
 class LRUCache {
 
-    private Map<Integer, Integer> map;
+    private final Map<Integer, Node> map;
+    private final Node head;
+    private final Node tail;
+    private final int capacity;
 
     public LRUCache(int capacity) {
-        this.map = new LinkedHashMap<>(capacity, 0.75f, true) {
-            @Override
-            protected boolean removeEldestEntry(Map.Entry eldest) {
-                return size() > capacity;
-            }
-        };
+
+        this.map = new HashMap<>();
+        this.head = new Node(0, 0);
+        this.tail = new Node(0, 0);
+        this.capacity = capacity;
+
+        head.next = tail;
+        tail.prev = head;
     }
     
     public int get(int key) {
-        return map.getOrDefault(key, -1);
+        
+        if (!map.containsKey(key)) {
+            return -1;
+        }
+
+        var node = map.get(key);
+
+        remove(node);
+        insert(node);
+
+        return node.value;
     }
     
     public void put(int key, int value) {
-        map.put(key, value);
+        
+        if (map.containsKey(key)) {
+            remove(map.get(key));
+        }
+
+        if (map.size() == capacity) {
+            remove(tail.prev);
+        }
+
+        insert(new Node(key, value));
+    }
+
+    private void remove(Node node) {
+        map.remove(node.key);
+        node.prev.next = node.next;
+        node.next.prev = node.prev;
+    }
+
+    private void insert(Node node) {
+        map.put(node.key, node);
+        node.next = head.next;
+        node.next.prev = node;
+        head.next = node;
+        node.prev = head;
+    }
+
+    private static class Node {
+
+        private Node next;
+        private Node prev;
+        private int key;
+        private int value;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
     }
 }
