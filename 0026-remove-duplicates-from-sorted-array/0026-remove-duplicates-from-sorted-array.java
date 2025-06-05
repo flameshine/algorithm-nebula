15b1fe0e81ecf9aca9174ebfc3b5954cbfbc59ee
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-06-05 23:26:07 +0200

    Time: 0 ms (100%), Space: 44.8 MB (78.25%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,15 +1,15 @@
 class Solution {
 
     public int removeDuplicates(int[] nums) {
-        
-        var result = 1;
+
+        var pointer = 1;
 
         for (var i = 1; i < nums.length; i++) {
             if (nums[i] != nums[i - 1]) {
-                nums[result++] = nums[i];
+                nums[pointer++] = nums[i];
             }
         }
 
-        return result;
+        return pointer;
     }
 }
