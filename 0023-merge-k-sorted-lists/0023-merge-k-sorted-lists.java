6c4c6462f2e3d24e96b174e7ad3087c0006cca6b
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-06-09 17:57:32 +0200

    Time: 4 ms (74.16%), Space: 44.5 MB (64.24%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -1,58 +1,29 @@
 class Solution {
-
+    
     public ListNode mergeKLists(ListNode[] lists) {
+        
+        var dummy = new ListNode(-1);
+        var prev = dummy;
 
-        if (lists == null || lists.length == 0) {
-            return null;
-        }
+        Queue<ListNode> minHeap = new PriorityQueue<>((a, b) -> a.val - b.val);
 
-        return merge(lists, 0, lists.length - 1);
-    }
-
-    private ListNode merge(ListNode[] lists, int start, int end) {
-
-        if (start == end) {
-            return lists[start];
-        }
-
-        if (start + 1 == end) {
-            return merge(lists[start], lists[end]);
-        }
-
-        var middle = start + (end - start) / 2;
-        var left = merge(lists, start, middle);
-        var right = merge(lists, middle + 1, end);
-
-        return merge(left, right);
-    }
-
-    private ListNode merge(ListNode l1, ListNode l2) {
-
-        if (l1 == null) {
-            return l2;
-        }
-
-        if (l2 == null) {
-            return l1;
-        }
-
-        var dummy = new ListNode(0);
-        var pointer = dummy;
-
-        while (l1 != null && l2 != null) {
-
-            if (l1.val < l2.val) {
-                pointer.next = l1;
-                l1 = l1.next;
-            } else {
-                pointer.next = l2;
-                l2 = l2.next;
+        for (var l : lists) {
+            if (l != null) {
+                minHeap.offer(l);
             }
-
-            pointer = pointer.next;
         }
 
-        pointer.next = l1 == null ? l2 : l1;
+        while (minHeap.size() > 0) {
+
+            var current = minHeap.poll();
+            
+            prev.next = current;
+            prev = prev.next;
+
+            if (current.next != null) {
+                minHeap.offer(current.next);
+            }
+        }
 
         return dummy.next;
     }
