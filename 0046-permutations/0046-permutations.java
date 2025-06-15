54c860912a6352cb64a53784021c7a8523784fd5
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-06-15 22:18:38 +0200

    Time: 2 ms (39.12%), Space: 44.3 MB (91.74%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -2,21 +2,26 @@ class Solution {
 
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        permute(nums, 0, result, new ArrayList<>());
+        permute(nums, result, new ArrayList<>());
         return result;
     }
 
-    private static void permute(int[] nums, int index, List<List<Integer>> result, List<Integer> current) {
+    private static void permute(int[] nums, List<List<Integer>> result, List<Integer> current) {
 
         if (current.size() == nums.length) {
-            result.add(current);
+            result.add(new ArrayList<>(current));
             return;
         }
 
-        for (var i = 0; i <= current.size(); i++) {
-            List<Integer> permutation = new ArrayList<>(current);
-            permutation.add(i, nums[index]);
-            permute(nums, index + 1, result, permutation);
+        for (var i = 0; i < nums.length; i++) {
+
+            if (current.contains(nums[i])) {
+                continue;
+            }
+
+            current.add(nums[i]);
+            permute(nums, result, current);
+            current.removeLast();
         }
     }
 }
