06a55e11dcc6b414381083abc7dac93d02003765
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-06-19 12:20:01 +0200

    Time: 2 ms (83.69%), Space: 44.6 MB (64.33%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -2,33 +2,31 @@ class Solution {
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result = new ArrayList<>();
-        createCombination(candidates, target, 0, new ArrayList<>(), result);
+        combinationSum(candidates, target, result, new ArrayList<>(), 0);
         return result;
     }
 
-    private static void createCombination(
+    private static void combinationSum(
         int[] candidates,
-        int target,
-        int index,
+        int remaining,
+        List<List<Integer>> accumulator,
         List<Integer> combination,
-        List<List<Integer>> result
+        int index
     ) {
-
-        if (target == 0) {
-            result.add(new ArrayList<>(combination));
+        
+        if (remaining < 0) {
             return;
         }
 
-        if (index == candidates.length) {
+        if (remaining == 0) {
+            accumulator.add(new ArrayList<>(combination));
             return;
         }
 
-        if (candidates[index] <= target) {
-            combination.add(candidates[index]);
-            createCombination(candidates, target - candidates[index], index, combination, result);
-            combination.remove(combination.size() - 1);
+        for (var i = index; i < candidates.length; i++) {
+            combination.add(candidates[i]);
+            combinationSum(candidates, remaining - candidates[i], accumulator, combination, i);
+            combination.removeLast();
         }
-
-        createCombination(candidates, target, index + 1, combination, result);
     }
 }
