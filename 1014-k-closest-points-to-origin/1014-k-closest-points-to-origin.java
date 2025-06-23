4a970cc5ed118a03c2f92750185e4f2b51811e56
Anton Pokhyla <57772059+flameshine@users.noreply.github.com>
2025-06-23 20:23:33 +0200

    Time: 31 ms (55.63%), Space: 50.8 MB (79.69%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -2,19 +2,27 @@ class Solution {
 
     public int[][] kClosest(int[][] points, int k) {
         
-        Queue<int[]> heap = new PriorityQueue<>((a, b) -> a[0] * a[0] + a[1] * a[1] - b[0] * b[0] - b[1] * b[1]);
+        Queue<int[]> heap = new PriorityQueue<>(Solution::euclideanDistance);
 
-        for (var point : points) {
-            heap.offer(point);
+        for (var p : points) {
+
+            heap.offer(p);
+
+            if (heap.size() > k) {
+                heap.poll();
+            }
         }
 
         var result = new int[k][2];
-        var i = 0;
 
-        while (k-- > 0) {
-            result[i++] = heap.poll();
+        while (k > 0) {
+            result[--k] = heap.poll();
         }
 
         return result;
+    }
+
+    private static int euclideanDistance(int[] a, int[] b) {
+        return b[0] * b[0] + b[1] * b[1] - a[0] * a[0] - a[1] * a[1];
     }
 }
